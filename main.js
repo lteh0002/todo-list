5bc027d26379d8295b110cb1de5fe450c9040024
lteh0002 <tehlinhui@gmail.com>
2022-11-25 12:30:07 +0800

    Added test code for task button to append content

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";document.querySelector(".allButton").addEventListener("click",(function(){const t=document.querySelector(".list-content"),e=document.createElement("p");e.textContent="Hi is this working",t.appendChild(e)}))})();
